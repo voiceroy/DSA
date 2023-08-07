897b59bf25c01cc8d5f2f0a9a45786c562dd1390
Harishankar G <harishankargaddanakeri@gmail.com>
2023-08-07 19:51:03 +0530

    Fix the description in the selection sort

--- Go/Algorithms/SelectionSort.go
+++ Go/Algorithms/SelectionSort.go
@@ -2,16 +2,16 @@ package algorithms
 
 func SelectionSort(array []int) {
 	/*
-		Bubble sort, is a simple sorting algorithm that repeatedly steps through the input list element by element, comparing the current element with the one after it, swapping their values if needed.
-		These passes through the list are repeated until no swaps had to be performed during a pass, meaning that the list has become fully sorted.
-		The algorithm, which is a comparison sort, is named for the way the larger elements "bubble" up to the top of the list.
+	   Selection sort is an in-place comparision sorting algorithm. The algorithm divides the input list into two parts:
+	   a sorted sublist of items which is built up from left to right at the front (left) of the list and a sublist of the remaining unsorted items that occupy the rest of the list.
+	   Initially, the sorted sublist is empty and the unsorted sublist is the entire input list. The algorithm proceeds by finding the smallest (or largest, depending on sorting order) element in the unsorted sublist,
+	   exchanging (swapping) it with the leftmost unsorted element (putting it in sorted order), and moving the sublist boundaries one element to the right.
 
-		Time Complexity: - Worst Case:   O(n^2) (quadratic)
-						 - Average Case: O(n^2) (quadratic)
-			             - Best Case:    O(n)   (linear) {Only if it's optimized for the best case scenario}
+	   Time Complexity: - Worst Case:    O(n^2) (quadratic)
+	                    - Average Case:  O(n^2) (quadratic)
+	                    - Best Case:     O(n^2) (quadratic) {Has to compare each element with all the others}
 
-
-		Space Complexity: - All Cases:   O(1)   (constant) {In place}
+	   Space Complexity: - All Cases:    O(1)   (constant) {In-place sorting algorithm}
 	*/
 
 	for i := range array { // Go through each index
